b9f9ce9b76d98367a3e55f5a7f23da61d781f09b
CaptainSP <19982002ke@gmail.com>
2023-03-17 16:35:17 +0300

    Created delete and edit comment methods

--- src/modules/controllers/comment-controller.ts
+++ src/modules/controllers/comment-controller.ts
@@ -19,6 +19,45 @@ export default class CommentController extends BaseController {
       });
       next();
     });
+
+    router.delete("/:id", async (req, res, next) => {
+      const result = await this.deleteComment(req.params.id);
+
+      res.send({
+        success: true,
+        data: result,
+      });
+      next();
+    });
+
+    router.post("/:id/edit", async (req, res, next) => {
+      const result = await this.editComment(req.params.id, req.body.message);
+
+      res.send({
+        success: true,
+        data: result,
+      });
+      next();
+    });
+  }
+
+  private async deleteComment(commentID: string) {
+    const comment = await CommentModel.findById(OID(commentID));
+    await comment.remove();
+    await PostModel.updateOne(
+      { _id: comment.post },
+      { $pull: { comments: OID(commentID) } }
+    );
+
+    return comment;
+  }
+
+  //edit comment
+  public async editComment(commentID: string, message: string) {
+    this.validateComment(message);
+    const comment = await CommentModel.findById(OID(commentID));
+    const updatedComment = await comment.save();
+    return updatedComment;
   }
 
   private async createComment(postID: string, userID, message: string) {
@@ -33,15 +72,16 @@ export default class CommentController extends BaseController {
     const savedComment = await newComment.save();
 
     //Add comment to post
-    PostModel.updateOne({ _id: postID }, { $push: { comments: savedComment._id } })
-
+    PostModel.updateOne(
+      { _id: postID },
+      { $push: { comments: savedComment._id } }
+    );
 
     return savedComment;
   }
 
   // Be sure message length is more than 4 and less than 250
   private async validateComment(message: string) {
-
     if (message.length < 4 || message.length > 250) {
       throw new Error("Comment must be between 4 and 250 characters");
     }
