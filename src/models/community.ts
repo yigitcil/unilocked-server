018753b9e76bab56f318a81123fbdf0c331c5b84
CaptainSP <19982002ke@gmail.com>
2023-03-31 17:46:44 +0300

    Added community model

---
+++ src/models/community.ts
@@ -0,0 +1,37 @@
+import { User } from "@models/user";
+import { getModelForClass, Ref } from "@typegoose/typegoose";
+import { prop } from "@typegoose/typegoose/lib/prop";
+import { Post } from "./post";
+
+export class Community {
+  @prop()
+  name: string;
+  @prop()
+  summary: string;
+  @prop()
+  description: string;
+
+  @prop()
+  backdrop: string;
+
+  @prop()
+  poster: string;
+
+  @prop()
+  tags: string[];
+
+  @prop({ ref: () => User })
+  followers: Ref<User>;
+
+  @prop({ ref: () => User })
+  contributors: Ref<User>[];
+
+  @prop({ ref: () => User })
+  likes: Ref<User>[];
+  @prop({ type: () => [String] })
+  links: string[];
+}
+
+const CommunityModel = getModelForClass(Community);
+
+export { CommunityModel };
