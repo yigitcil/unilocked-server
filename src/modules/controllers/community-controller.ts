423aea0d39a7a8a95dcf163e2b21ea75a5d611a5
CaptainSP <19982002ke@gmail.com>
2023-04-02 21:32:33 +0300

    Changed express user model

---
+++ src/modules/controllers/community-controller.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import BaseController from "./base-controller";
+
+export default class CommunityController extends BaseController {
+    listen(router: Router): void {
+        
+    }
+
+}
