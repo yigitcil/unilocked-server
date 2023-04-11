aa4614c9f31ea98cde098262b8cccc8dae15948c
CaptainSP <19982002ke@gmail.com>
2023-04-12 00:32:23 +0300

    Restricted society permissions

--- src/modules/controllers/society-controller.ts
+++ src/modules/controllers/society-controller.ts
@@ -4,12 +4,14 @@ import PaginateService from "@modules/services/paginate";
 import { Router } from "express";
 import mongoose from "mongoose";
 import BaseController from "./base-controller";
+import { param } from "express-validator";
+import ensureAuthorized from "@modules/middleware/ensure-authorized";
 
 export class SocietyController extends BaseController {
     listen(router: Router): void {
   
       //Get society by ID
-      router.get("/:id", async (req, res, next) => {
+      router.get("/:id", ensureAuthorized('societies.view'), param('id').isMongoId(),async (req, res, next) => {
         const society = await this.byId(req.params.id);
         res.send({
           sucess: true,
@@ -19,7 +21,7 @@ export class SocietyController extends BaseController {
       });
 
       //Get socities
-      router.get("/", async (req, res, next) => {
+      router.get("/",ensureAuthorized('societies.view'), async (req, res, next) => {
         const socities = SocietyModel.find();
   
         res.send(success(await PaginateService.paginate(req, SocietyModel, socities)));
