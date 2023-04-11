73e63b66de3beb93516175073f25102c8da27c70
CaptainSP <19982002ke@gmail.com>
2023-04-11 23:39:12 +0300

    Configured redis

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import { Connector } from "@modules/connector";
 
 import { Router } from "@modules/router";
@@ -7,6 +6,7 @@ import { Express } from "express";
 import { Server } from "@modules/server";
 import mongoose from "mongoose";
 import { User as UserModel } from "@models/user";
+import { RedisService } from "@modules/services/redis";
 
 declare global {
   namespace Express {
@@ -16,22 +16,19 @@ declare global {
   }
 }
 
-require('dotenv/config')
+require("dotenv/config");
 
-process.env.APP_PATH = "C:/Users/Administrator/Desktop/"
-process.env.path = "C:/Users/Administrator/Desktop/"
+process.env.APP_PATH = "C:/Users/Administrator/Desktop/";
+process.env.path = "C:/Users/Administrator/Desktop/";
 
 const connector = new Connector();
 connector.connect(() => {
- 
-
-    const server = new Server();
-    server.listen(3000, (app: Express) => {
-      const router = new Router(app);
-      router.listen();
-      
-    });
- 
+  const server = new Server();
+  server.listen(3000, (app: Express) => {
+    RedisService.init();
+    const router = new Router(app);
+    router.listen();
+  });
 });
 
 process
