290ebba83e5f5fbb0f9702a001fc46bfd51c5b2c
CaptainSP <19982002ke@gmail.com>
2023-04-11 23:50:25 +0300

    Added express validator

--- src/modules/controllers/comment-controller.ts
+++ src/modules/controllers/comment-controller.ts
@@ -4,10 +4,23 @@ import { OID } from "@modules/helpers/generate-object-id";
 import { Router } from "express";
 import { use } from "passport";
 import BaseController from "./base-controller";
+import { body, checkSchema, param } from "express-validator";
+import { tr } from "@modules/services/translator";
 
 export default class CommentController extends BaseController {
   listen(router: Router): void {
-    router.post("/:id", async (req, res, next) => {
+    router.post("/:id",checkSchema({
+      message: {
+        isLength: {
+          options: { min: 1 },
+        },
+        errorMessage: tr("Comment must be at least 1 character"),
+      },
+      id : {
+        isMongoId: true,
+        errorMessage: tr("Invalid post id")
+      }
+    }), async (req, res, next) => {
       const comment = await this.createComment(
         req.params.id,
         req.user._id,
@@ -21,7 +34,7 @@ export default class CommentController extends BaseController {
       next();
     });
 
-    router.delete("/:id", async (req, res, next) => {
+    router.delete("/:id", param('id').isMongoId(),async (req, res, next) => {
       const result = await this.deleteComment(req.params.id);
 
       res.send({
@@ -31,7 +44,18 @@ export default class CommentController extends BaseController {
       next();
     });
 
-    router.post("/:id/edit", async (req, res, next) => {
+    router.post("/:id/edit",checkSchema({
+      message: {
+        isLength: {
+          options: { min: 1 },
+        },
+        errorMessage: tr("Comment must be at least 1 character"),
+      },
+      id : {
+        isMongoId: true,
+        errorMessage: tr("Invalid post id")
+      }
+    }), async (req, res, next) => {
       const result = await this.editComment(req.params.id, req.body.message);
 
       res.send({
@@ -41,7 +65,16 @@ export default class CommentController extends BaseController {
       next();
     });
 
-    router.post("/:id/:reaction", async (req, res, next) => {
+    router.post("/:id/:reaction",checkSchema({
+      id : {
+        isMongoId: true,
+        errorMessage: tr("Invalid post id")
+      },
+      reaction : {
+        isString: true,
+        errorMessage: tr("Invalid reaction")
+      }
+    }), async (req, res, next) => {
       if (req.params.reaction !== "like" && req.params.reaction !== "dislike")
         res.status(404).send({ success: false, error: "Invalid reaction" });
       else {
