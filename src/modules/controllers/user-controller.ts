805465d464f2f7c3428d0a2f5ae996824212583a
Utku Oktay <utkuoktay06@gmail.com>
2023-07-15 23:29:13 +0300

    Added get company name endpoint

--- src/modules/controllers/user-controller.ts
+++ src/modules/controllers/user-controller.ts
@@ -3,13 +3,18 @@ import { Router } from "express";
 import mongoose from "mongoose";
 import BaseController from "./base-controller";
 import { UserModel } from "../../resolved-models";
+import ensureAuthorized from "../middleware/ensure-authorized";
 
 
 export class UserController extends BaseController{
 
-
   listen(router: Router): void {
-   
+    router.get("/:userID/company", ensureAuthorized("company.view"), async (req, res, next) => {
+      const companyName = this.getCompanyName(req.params.userID);
+
+      res.send({ success: true, companyName: companyName});
+      next();
+    });
   }
 
   public byEmail(email: string) {
@@ -21,5 +26,9 @@ export class UserController extends BaseController{
 
   public byUsername(username: string) {
     return UserModel.findOne({ username: username });
+  }
+
+  public getCompanyName(id: string) {
+    this.byId(id).populate("company").name;
   }
 }
