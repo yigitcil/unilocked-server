87104663ee9bc6e8ff2007ecf5d2d9095c87c9dc
yigitcil <yigit.dsi.9@gmail.com>
2023-09-21 22:40:49 +0300

    sadsa

--- build/modules/controllers/user-controller.js
+++ build/modules/controllers/user-controller.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,8 +16,14 @@ exports.UserController = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const base_controller_1 = __importDefault(require("./base-controller"));
 const resolved_models_1 = require("../../resolved-models");
+const ensure_authorized_1 = __importDefault(require("../middleware/ensure-authorized"));
 class UserController extends base_controller_1.default {
     listen(router) {
+        router.get("/:userID/company", (0, ensure_authorized_1.default)("company.view"), (req, res, next) => __awaiter(this, void 0, void 0, function* () {
+            const companyName = this.getCompanyName(req.params.userID);
+            res.send({ success: true, companyName: companyName });
+            next();
+        }));
     }
     byEmail(email) {
         return resolved_models_1.UserModel.findOne({ email: email });
@@ -18,6 +33,9 @@ class UserController extends base_controller_1.default {
     }
     byUsername(username) {
         return resolved_models_1.UserModel.findOne({ username: username });
+    }
+    getCompanyName(id) {
+        this.byId(id).populate("company").name;
     }
 }
 exports.UserController = UserController;
--- build/modules/router.js
+++ build/modules/router.js
@@ -1,41 +1,46 @@
-'use strict';
-var __importDefault = this && this.__importDefault || function (mod) {
-    return mod && mod.__esModule ? mod : { 'default': mod };
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, '__esModule', { value: true });
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.Router = void 0;
-const auth_controller_1 = __importDefault(require('./controllers/auth-controller'));
-const user_profile_controller_1 = require('./controllers/user-profile-controller');
-const express_1 = require('express');
-const society_controller_1 = require('./controllers/society-controller');
-const project_controller_1 = require('./controllers/project-controller');
-const post_controller_1 = require('./controllers/post-controller');
-const event_controller_1 = require('./controllers/event-controller');
-const message_controller_1 = require('./controllers/message-controller');
-const home_controller_1 = __importDefault(require('./controllers/home-controller'));
-const upload_controller_1 = require('./controllers/upload-controller');
+const auth_controller_1 = __importDefault(require("../modules/controllers/auth-controller"));
+const user_profile_controller_1 = require("../modules/controllers/user-profile-controller");
+const express_1 = require("express");
+const society_controller_1 = require("./controllers/society-controller");
+const project_controller_1 = require("./controllers/project-controller");
+const post_controller_1 = require("./controllers/post-controller");
+const event_controller_1 = require("./controllers/event-controller");
+const message_controller_1 = require("./controllers/message-controller");
+const home_controller_1 = __importDefault(require("./controllers/home-controller"));
+const user_recommendation_controller_1 = require("./controllers/user-recommendation-controller");
+const company_controller_1 = __importDefault(require("./controllers/company-controller"));
+const upload_controller_1 = require("./controllers/upload-controller");
 class Router {
     constructor(app) {
         this.app = app;
     }
     listen() {
-        this.createRoute('auth', auth_controller_1.default);
-        this.createRoute('user-profile', user_profile_controller_1.UserProfileController);
-        this.createRoute('society', society_controller_1.SocietyController);
-        this.createRoute('project', project_controller_1.ProjectController);
-        this.createRoute('posts', post_controller_1.PostController);
-        this.createRoute('event', event_controller_1.EventController);
-        this.createRoute('message', message_controller_1.MessageController);
-        this.createRoute('home', home_controller_1.default);
+        this.createRoute("auth", auth_controller_1.default);
+        this.createRoute("user-profile", user_profile_controller_1.UserProfileController);
+        this.createRoute("society", society_controller_1.SocietyController);
+        this.createRoute("project", project_controller_1.ProjectController);
+        this.createRoute("posts", post_controller_1.PostController);
+        this.createRoute("event", event_controller_1.EventController);
+        this.createRoute("message", message_controller_1.MessageController);
+        this.createRoute("home", home_controller_1.default);
+        this.createRoute("user-recommendation", user_recommendation_controller_1.UserRecommentationController);
+        this.createRoute("company", company_controller_1.default);
         // init upload controler
         new upload_controller_1.UploadController(this.app).listen();
     }
-    createRoute(path = null, controller, base = '/api/') {
+    createRoute(path = null, controller, base = "/api/") {
         const router = (0, express_1.Router)();
         new controller().listen(router);
         if (path) {
             this.app.use(base + path, router);
-        } else {
+        }
+        else {
             this.app.use(base, router);
         }
     }
